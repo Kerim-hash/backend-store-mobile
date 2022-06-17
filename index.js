00377acb9d1deac4af1b88b93ceaf7313ae6d2a4
Kerim <kerim.nurkaliev@sibers.com>
2022-06-17 14:52:02 +0600

    deploy

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ const orderRouter = require('./routes/orders')
 const authJwt = require('./helpers/jwt')
 const res = require('express/lib/response')
 
-// промежуточное программное обеспечение
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
-// app.use(authJwt());
-// mongoose connect
 mongoose
     .connect(`mongodb+srv://kerim:kera2003kera@cluster0.bvnpd.mongodb.net/?retryWrites=true&w=majority`, {
         useNewUrlParser: true, useUnifiedTopology: true 
     })
     .then((res) => console.log("Connected to DB"))
     .catch((error) => console.log('connection error'));
+
+// промежуточное программное обеспечение
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+// app.use(authJwt());
+// mongoose connect
 // routes
 app.use('/api/products', productRouter)
 app.use('/api/category', CategoryRouter)
