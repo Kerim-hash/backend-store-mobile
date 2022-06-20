e6a16643d5b01f3d85cfa94e6e95eb2feaac0ef4
Kerim <kerim.nurkaliev@sibers.com>
2022-06-20 12:33:13 +0600

    deploy

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { UserModel } = require("../models/user.model");
+const UserModel= require("./../models/user.model");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
