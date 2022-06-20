dd142118fd8eadce98b8242ce488c7455c0933df
Kerim <kerim.nurkaliev@sibers.com>
2022-06-20 14:04:53 +0600

    deploy

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ const authJwt = require("./helpers/jwt");
 const res = require("express/lib/response");
 
 
+
 // промежуточное программное обеспечение
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 // app.use(authJwt());
-
+// mongoose connect
+mongoose
+  .connect(
+    `mongodb+srv://kerim:kera2003kera@cluster0.bvnpd.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((res) => console.log("Connected to DB"))
+  .catch((error) => console.log("connection error"));
 // import
 const productRouter = require("./routes/product");
 const CategoryRouter = require("./routes/categories");
@@ -32,27 +43,17 @@ app.get("/", (req, res) => {
 
 let PORT = process.env.PORT || 5000
 
-// mongoose connect
-// mongoose
-//   .connect(
-//     `mongodb+srv://kerim:kera2003kera@cluster0.bvnpd.mongodb.net/?retryWrites=true&w=majority`,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
+
+// mongoose.connect(
+//   'mongodb+srv://kerim:kera2003kera@cluster0.bvnpd.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true,
+//         useUnifiedTopology: true},
+//   (err) => {
+//     if (err) {
+//       console.error('DB: fail');
+//       console.error(err.message);
 //     }
-//   )
-//   .then((res) => console.log("Connected to DB"))
-//   .catch((error) => console.log("connection error"));
-mongoose.connect(
-  'mongodb+srv://kerim:kera2003kera@cluster0.bvnpd.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true,
-        useUnifiedTopology: true},
-  (err) => {
-    if (err) {
-      console.error('DB: fail');
-      console.error(err.message);
-    }
-    console.log('DB: connected');
+//     console.log('DB: connected');
   
   
-    app.listen(PORT, () => console.log('blog server running on port', PORT));
-  });
+//   });
+  app.listen(PORT, () => console.log('blog server running on port', PORT));
